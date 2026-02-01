7fdde934f5f21bebdcbe2a445c0516299c6cbf9b
vinaykondru <vinaykumarkondru@gmail.com>
2026-02-01 23:04:40 +0530

    chrome

--- conftest.py
+++ conftest.py
@@ -3,12 +3,17 @@ import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 @pytest.fixture(scope="class")
 def browser_setup(request):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument("--no-sandbox")
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=chrome_options)
+    # driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     request.cls.driver = driver
     yield
